3c1b63059dddd727dda61c606340aeae28a55aaf
s_ito <s_ito@okwave.co.jp>
2018-11-06 16:35:34 +0900

    Add any

---
+++ scripts/AfterInstall.sh
@@ -0,0 +1,10 @@
+#/bin/sh
+
+chmod 777 tmp
+
+#/usr/bin/php artisan storage:link
+#/usr/bin/php artisan view:clear
+#/usr/bin/php artisan cache:clear
+#/usr/bin/php artisan config:cache
+#/usr/bin/php artisan optimize
+#/usr/bin/php artisan migrate
